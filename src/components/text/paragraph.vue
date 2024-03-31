8528b478220145bd9c21eba1ccb7e29f11b52c67
Volodymyr Lysenko <morgota199@gmail.com>
2024-04-01 02:05:39 +0300

    Add paragraph

---
+++ src/components/text/paragraph.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import { defineProps } from 'vue'
+
+    defineProps({
+        text: {
+            type: String,
+            default: '',
+        },
+    })
+</script>
+
+<template>
+    <div class="paragraph">
+        <span v-text="text" />
+    </div>
+</template>
+
+<style lang="scss">
+    @import '@/assets/scss/paragraph.scss';
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import LinkButton from './components/buttons/link-button.vue'
 import Heading2 from './components/text/heading-2.vue'
 import Heading3 from './components/text/heading-3.vue'
+import Paragraph from './components/text/paragraph.vue'
 
-export { LinkButton, Heading2, Heading3 }
+export { LinkButton, Heading2, Heading3, Paragraph }
