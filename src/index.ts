a4de6248777974b256493068856e653273b6c2af
Volodymyr Lysenko <morgota199@gmail.com>
2024-04-01 01:31:22 +0300

    Add headings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import LinkButton from './components/ui/link-button.vue'
+import LinkButton from './components/buttons/link-button.vue'
+import Heading2 from './components/text/heading-2.vue'
+import Heading3 from './components/text/heading-3.vue'
 
-export { LinkButton }
+export { LinkButton, Heading2, Heading3 }
